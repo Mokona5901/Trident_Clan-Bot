bd5247a2a088929605f3bacb4e5de1f2b7d184fb
Mokona5901 <82078037+Mokona5901@users.noreply.github.com>
2023-05-05 12:03:46 +0200

    typo fail on /news markdown

--- index.js
+++ index.js
@@ -149,10 +149,10 @@ client.on('interactionCreate', async interaction =>
 				  });*/
 				const tournamentEmbed = new EmbedBuilder()
 				.addFields(
-                	{ name: '__ACEs Tournament #3', value: ' ' },
+                	{ name: '__ACEs Tournament #3__', value: ' ' },
                 	{ name: '__Description :__', value: 'For that tournament, you will only be able to use event forms pokemons.' },
 					{ name: '__Requirements :__', value: '> One Gmax Pokémon maximum\n > One Mega Pokémon maximum\n > One Legendary/Mythical Pokémon maximum\n > One Easter event Pokémon maximum\n > God Pokémon are banned\n > No illegal pokemons allowed :minion_stare~1:'},
-                	{ name: '_Number of contestants :__', value: '8' },
+                	{ name: '__Number of contestants :__', value: '8' },
                 	{ name: '__When ?__', value: '<t:1683880200:F>' },
 					{ name: '__Prizes__', value: '> 1st : Good IV Legendary/Mythical + 25k\n > 2nd : Mid IV Legendary/Mythical + 20k' });
 
